f3300e627e193a843387470a668d4eace544aa72
Connor Smith <134646469+nthconnor@users.noreply.github.com>
2024-09-30 14:34:09 -0400

    login tests working

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 
 import User from "../models/userModel.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
@@ -26,22 +27,50 @@ export const signup = async (req, res) => {
       avatar,
     });
 
-    await newUser.save();
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
 
-    res.status(201).json({
-      _id: newUser._id,
-      displayName: newUser.displayName,
-      username: newUser.username,
-      avatar: newUser.avatar,
-    });
+      res.status(201).json({
+        _id: newUser._id,
+        displayName: newUser.displayName,
+        username: newUser.username,
+        avatar: newUser.avatar,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
   } catch (error) {
     console.log("Error in sign up control", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
 
-export const login = (req, res) => {
-  res.send("login route");
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username of password" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      displayName: user.displayName,
+      username: user.username,
+      avatar: user.avatar,
+    });
+  } catch (error) {
+    console.log("error in login controller", error.message);
+    res.status(500).json({ error: "Internal server rror" });
+  }
 };
 
 export const logout = (req, res) => {
