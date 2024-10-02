95b8a20a8a91496f95766b1a7ca7abdef3be752d
Connor Smith <134646469+nthconnor@users.noreply.github.com>
2024-10-02 19:00:29 -0400

    test

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -7,7 +7,7 @@ const generateTokenAndSetCookie = (userId, res) => {
 
     res.cookie("jwt", token, {
         maxAge: 15 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
+        httpOnly: false,
         sameSite: "lax",
         secure: process.env.NODE_ENV === "production"
     })
