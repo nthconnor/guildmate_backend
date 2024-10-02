6121ffefb22736cc04d637668e7d13758fa7d6f9
Connor Smith <134646469+nthconnor@users.noreply.github.com>
2024-10-01 23:44:52 -0400

    cors

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ import authRoutes from "./routes/authRoutes.js"
 import guildRoutes from "./routes/guildRoutes.js"
 import connectToMongo from "./models/index.js"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}))
 app.use(cookieParser());
 app.use(express.json());
 app.use('/api/auth', authRoutes)
