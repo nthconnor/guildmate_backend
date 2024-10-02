cb826e30b7991d1b41afaa712927cf9bbef5b526
Connor Smith <134646469+nthconnor@users.noreply.github.com>
2024-10-02 15:47:14 -0400

    can get guild by Id, test works

--- controllers/guildController.js
+++ controllers/guildController.js
@@ -29,15 +29,35 @@ export const createGuild = async (req, res) => {
   }
 };
 
+export const getGuildById = async (req, res) => {
+  try {
+    const guildId = req.params.id;
+    const guild = await Guild.findById(guildId).populate(
+      "owner members"
+    );
+
+    if (!guild) {
+      return res.status(404).json({ error: "Guild not found" });
+    }
+
+    res.json(guild);
+  } catch (error) {
+    console.error("Error fetching guild:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
 export const getUserGuilds = async (req, res) => {
   try {
     const userId = req.params.id;
-    const guilds = await Guild.find({ members: userId }).populate("owner members");
+    const guilds = await Guild.find({ members: userId }).populate(
+      "owner members"
+    );
 
     if (!guilds || guilds.length === 0) {
       return res.status(404).json({ error: "No guilds found for this user" });
     }
-    
+
     res.json(guilds);
   } catch (error) {
     console.error("Error fetching user guilds:", error.message);
--- routes/guildRoutes.js
+++ routes/guildRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createGuild, getUserGuilds } from "../controllers/guildController.js";
+import { createGuild, getGuildById ,getUserGuilds } from "../controllers/guildController.js";
 import { authenticateJWT } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post('/create', authenticateJWT, createGuild)
+router.get("/:id", getGuildById) 
 router.get('/users/:id', authenticateJWT, getUserGuilds) // maybe move to user routes
 
 export default router;
